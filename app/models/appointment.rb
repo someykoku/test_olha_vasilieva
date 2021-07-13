adf2ef8ca259e1cc5cba95be7c9e4e9faaab3163
olha <olhavasilieva@gmail.com>
2021-07-13 13:47:35 +0300

    Add validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,13 @@
 class Appointment < ApplicationRecord
-    belongs_to :doctor, class_name: 'User', foreign_key: 'doctor_id'
-    belongs_to :patient, class_name: 'User', foreign_key: 'user_id'
+  validate :doctor_can_have_ten_patients
+  belongs_to :doctor, class_name: 'User', foreign_key: 'doctor_id'
+  belongs_to :patient, class_name: 'User', foreign_key: 'user_id'
+
+  private
+
+  def doctor_can_have_ten_patients
+    if new_record? && doctor.patients.count >= 10
+      errors.add(:patients_count, "Doctor has no free appointments")
+    end
+  end
 end
