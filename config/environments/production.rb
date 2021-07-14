dba52b9d4bcf38ada8b21ab8807b266fefb7e963
olha <olhavasilieva@gmail.com>
2021-07-14 16:30:15 +0300

    heroku debug logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,11 +87,12 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
+  
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger.level = Logger::DEBUG
   end
 
   # Do not dump schema after migrations.
